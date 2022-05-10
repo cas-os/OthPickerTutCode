34a3c025ff5e060f30b2b3243384f51698f21b42
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-10 14:56:25 +0200

    Initial Commit

---
+++ OthPickerTutCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OthPickerTutCode
+//
+//  Created by HAL-9001 on 10/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OthPickerTutCode/OthPickerTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthPickerTutCodeApp.swift
+//  OthPickerTutCode
+//
+//  Created by HAL-9001 on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct OthPickerTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
