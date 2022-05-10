21d6950a4d77c19b59c5fc343dbff8127b4f82a2
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-10 17:36:41 +0200

    /.1

--- OthPickerTutCode/ContentView.swift
+++ OthPickerTutCode/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colorNames = ["Black", "Red", "Green", "Blue"]
+    @State private var colorIndex = 0
+    @State private var rotation: Double = 0
+    @State private var text: String = "String in Rotate mode"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Spacer()
+                Text(text)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .rotationEffect(.degrees(rotation))
+                    .animation(.easeInOut, value: rotation)
+                    .foregroundColor(colors[colorIndex])
+                Spacer()
+                Slider(value: $rotation, in: 0...360, step: 0.1)
+                    .padding()
+                TextField("Enter the text :: ", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Picker(
+                    selection: $colorIndex,
+                    label: Text("Color")) { // nie widac tego tekstu?
+                        ForEach (0..<colorNames.count) {
+                            Text(colorNames[$0])
+                                .foregroundColor(colors[$0])
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    .padding()
+            }
+        }
     }
 }
 
